ff0a105f023a1909b0ad2032ddb505636e7511ad
Carol Zaglia <carolinagalvaosantos@gmail.com>
2020-07-31 09:24:09 -0300

    Removing docussaurus folder

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/home/jvmedeiros/Documentos/Trabalho/Zappts/jvcarol.github.io/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/jvmedeiros/Documentos/Trabalho/Zappts/jvcarol.github.io/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/jvmedeiros/Documentos/Trabalho/Zappts/jvcarol.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/home/jvmedeiros/Documentos/Trabalho/Zappts/jvcarol.github.io/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-  require("/home/jvmedeiros/Documentos/Trabalho/Zappts/jvcarol.github.io/src/css/custom.css"),
-];
